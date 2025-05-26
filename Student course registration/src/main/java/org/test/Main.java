85d0ec7dccd32d971ac9c78322f26edab844f608
NiharikaSwain0 <niharikaswain560@gmail.com>
2025-05-26 22:59:34 +0530

    Creating the new project with JDK & IDE setup

---
+++ Student course registration/src/main/java/org/test/Main.java
@@ -0,0 +1,7 @@
+package com.harsh.registration;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Student Course Registration System");
+    }
+}
